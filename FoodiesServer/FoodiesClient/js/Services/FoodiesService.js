92e0eb08c6aee1b5e00dab5563b359c5daf6ae04
LAPTOP-CHD41R4F\shira <yarin.andreev@gmail.com>
2017-08-09 15:39:08 +0300

    service angular

---
+++ FoodiesServer/FoodiesClient/js/Services/FoodiesService.js
@@ -0,0 +1,36 @@
+foodiesApp.factory('GetIngredientsService', ['$http', function ($http) {
+    this.Ingredients = function(userID){
+      return $http({ method: 'POST', url: 'http://localhost/AddIngredient', UserID: userID});
+  }
+}]);
+
+foodiesApp.factory('GetRecepiesService', ['$http', function ($http) {
+    this.Recepie = function(categoryID, sort){
+      return $http({ method: 'POST', url: 'http://localhost/GetRecepies', {CategotyId:categoryID, Sort:sort}});
+  }
+}]);
+
+foodiesApp.factory('ConnectUserService', ['$http', function ($http) {
+    this.User = function(UserName, Password){
+      return $http({ method: 'POST', url: 'http://localhost/AttemptLogin', {userName:UserName, password:Password}});
+  }
+}]);
+
+foodiesApp.factory('AddUserService', ['$http', function ($http) {
+    this.User = function(UserName, Password){
+      $http.put('http://localhost/AddUser',                                     
+          {},                                          
+          { params: { userName: UserName, password : Password } }   
+       );
+    }
+}]);
+
+foodiesApp.factory('AddIngredientService', ['$http', function ($http) {
+    this.Ingredient = function(ID, Name, PictureURL, Calories, UserID){
+      $http.put('http://localhost/AddIngredient',                                     
+          {},                                          
+          { params: { id: ID, name: Name, pictureUrl: PictureURL, calories: Calories, userId: UserID} }   
+       );
+    }
+}]);
+
